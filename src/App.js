2b22814b71011a2b67020cdabeef3a3f6bf12c48
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-26 12:32:24 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h2>Lets build gmail</h2>
+    </div>
+  );
+}
+
+export default App;
