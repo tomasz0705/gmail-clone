c49db47c144bb8ea2d268825b4933a59d75158c0
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-30 20:50:04 +0200

    Added header_left & header__middle containers to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h2>Lets build gmail</h2>
     </div>
   );
