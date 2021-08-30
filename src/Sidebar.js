54f5045bb87853719b65713e15bedb0b3bf4896f
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-30 21:03:57 +0200

    Finished Header.js

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.css';
+import { Button } from "@material-ui/core";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <Button>Compose</Button>
+        </div>
+    )
+}
+
+export default Sidebar
