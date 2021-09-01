ef0986930ab19ed41d27125e028d727ae642366c
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-01 19:48:07 +0200

    Finished emailList__sections > EmailList & Section component

--- src/EmailList.js
+++ src/EmailList.js
@@ -8,6 +8,10 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
 import SettingsIcon from '@material-ui/icons/Settings';
+import Section from './Section';
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 
 function EmailList() {
     return (
@@ -42,6 +46,12 @@ function EmailList() {
             </div>
 
             <div className="emailList__sections">
+                <Section Icon={InboxIcon} title="primary" color="red" selected />
+                <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+                <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+            </div>
+
+            <div className="emailList__list">
                 
             </div>
         </div>
