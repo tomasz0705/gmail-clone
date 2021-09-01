bdbb1f2cd4e7c5f2bd89cbf7197217e8f9dcd072
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-01 15:54:31 +0200

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail" component={Mail} />
+            <Route path="/" component={EmailList} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Mail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Mail;
